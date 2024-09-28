2d015374b8ae031e5ed284b4f611f5e8bdef409f
gigavivek <vivekshir6@gmail.com>
2024-09-28 12:43:45 +0530

    first commit

---
+++ new/src/src/rect.java
@@ -0,0 +1,20 @@
+package src;
+
+public class rect {
+	float rectangle;
+	float length;
+	float breadth;
+	public float getLength() {
+		return length;
+	}
+	public void setLength(float length) {
+		this.length = length;
+	}
+	public float getBreadth() {
+		return breadth;
+	}
+	public void setBreadth(float breadth) {
+		this.breadth = breadth;
+	}
+
+}
